4045058db88e8d79566b9d181e028ec95c00647c
DreCampbell <dillon-campbell@hotmail.com>
2022-11-14 19:01:59 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage':{
+        'age': 30,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLoaction': 'London, England'
+    },
+    'stormzy':{
+        'age': 29,
+        'birthName': 'Michael Ebenezer Kwadjo Omari Owuo Jr.',
+        'birthLoaction': 'London, England'
+    },
+    'dylan':{
+        'age': 30,
+        'birthName': 'Dylan',
+        'birthLoaction': 'Dylan'
+    }
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response)=>{
+    const rappersName = request.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+    response.json(rappers[rappersName])
+    }else{
+    response.json(rappers['dylan'])
+}
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on ${PORT}! You better go catch it!`)
+})
